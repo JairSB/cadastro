a9e3f82f8decdeddb3caf347370ea9439563b5a6
JairSB <benarrozjair@gmail.com>
2024-06-28 21:57:04 -0300

    verifica
    agora finalmente tem a validacão

--- jsb/js/cadastro.js
+++ jsb/js/cadastro.js
@@ -2,27 +2,65 @@ var salva = document.getElementById('salva');
 var conect =document.getElementById("connect");
 var nick=[];
 var senhas=[];
+//var teste=JSON.parse(sessionStorage.getItem("listadecadastro"));
+//console.log(teste);
 
 function lista_cadastrados(){
     let lista= document.getElementById('lista');
     lista.innerHTML ="";
-    nick.forEach(cada =>{
-        lista.innerHTML += `<li>${cada}<button>alterar</button><button>excluir</button></li>`;
+    nick.forEach((cada,ind) =>{
+        lista.innerHTML += `  <tr>
+            <th>${ind+1}</th>
+            <th>${cada}</th>
+            <th><button type="button" class="btn btn-outline-primary"><i class="bi bi-brush-fill"></i></button><button type="button" class="btn btn-outline-danger"><i class="bi bi-trash3"></i></button></th>
+        </tr>`
     })
 }
+function versetem(cra){
+    if( nick.indexOf(cra)<=0){
+        return true;
+    }else{
+        return false;
+    }
+}
+//function verifica(x){
+   // for(let i=0;i <1;)
+    //    nick.forEach(elem =>{
+      //      if(elem = x){
+       //         return true
+       //         i++;
+       //     }else
+        //    return false
+      //  })
+ //   }
 try{
     salva.addEventListener("click", evento=>{
         let nome = document.getElementById('nick');
         let senha = document.getElementById('senha');
-        nick.push(nome.value)
+       // if(verifica(nome)){
+       if((nome.value =="")||(senha.value=="")){
+        alert("falta algo");
+        focus(nome);
+       }else{
+        if(versetem(nome.value)){
+            alert("ja tem");
+        }else{
+            nick.push(nome.value)
         senhas.push(senha.value)
         nome.value = "";
         senha.value = "";
         console.log(nick,senhas);
-        //alert('cadastrado')
+        alert('cadastrado')
         //window.location.href="index2.html";
         lista_cadastrados()
-    })
+
+        }
+        
+       // window.sessionStorage.setItem("listadecadastro",JSON.stringify(nick));
+        //var teste=JSON.parse(sessionStorage.getItem("listadecadastro"));
+    //}else{
+    //}
+}})
 }catch(erro){
     console.log('ta procurando oq?')
 }
@@ -30,6 +68,11 @@ try{
     conect.addEventListener('click', even=>{
         let nome = document.getElementById('nick');
         let senha = document.getElementById('senha');
+       // if(verifica(nome)){
+            alert(`<a href="index.html">Cadastrar?</a>`)
+       // }else{
+            alert("connecta")
+       // }
         lista_cadastrados()
     })
 }catch(erro){
